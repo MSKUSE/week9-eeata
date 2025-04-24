62c933a437b7e9dbf933b68795dd42c9d8c0acdc
eylul <eylulelaata@posta.mu.edu.tr>
2025-04-24 13:17:33 +0300

    week9

---
+++ Abstract/src/testabstractshape.java
@@ -0,0 +1,30 @@
+import java.awt.*;
+
+public class testabstractshape {
+    public static void main(String[] args) {
+        Shape rectangle = new Rectangle (3,4)
+        System.out.println(rectangle.area());
+        System.out.println(rectangle.perimeter.());
+        System.out.println(rectangleI);
+        Rectangle rectangle = new Rectangle (3,4);
+        System.out.println(rectangle.area());
+        System.out.println(rectangle.perimeter.());
+        System.out.println(rectangleI);
+        Shape square = new Rectangle(3,3);
+        System.out.println(square.area());
+        System.out.println(square.perimeter.());
+        System.out.println(square);
+
+
+        ArrayList<Shape> shapes =new ArrayList<Shape>();
+        shapes.add(rectangle);
+        shapes,add(rectangle1);
+        shapes.add(square);
+
+        for (int i =0;i<3;i++){
+        System.out.println(shapes.get(i));
+    }
+
+
+    }
+}
--- Shape2D/src/TestShapes.java
+++ Shape2D/src/TestShapes.java
@@ -1,3 +1,5 @@
+
+import
 public class TestShapes {
     public static void main(String[] args) {
 
